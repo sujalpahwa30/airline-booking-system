337e686a8967258aa21b1c38a9f65ca0a0ebfe2a
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-07-20 00:26:20 +0530

    added new migration to add foreign key constraint

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+
+    // bad code alert 
+    // const { City, Airport } = require('./models');
+
+    // const bengaluru = await City.findByPk(1, {include: {model: Airport}});
+    // console.log(bengaluru);
 });
