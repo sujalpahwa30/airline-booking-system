6e51092edcd56509f3c012e47f6dd7dc5c5e3b57
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-07-22 03:01:51 +0530

    added complex join for fetching all airports

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -61,6 +61,7 @@ async function getAllFlights(query) {
         const flights = await flightRepository.getAllFlights(customFilter, sortFilter);
         return flights;
     } catch(error) {
+        console.log(error);
         throw new AppError('Cannot fetch data of all the flights', 
             StatusCodes.INTERNAL_SERVER_ERROR
         );
