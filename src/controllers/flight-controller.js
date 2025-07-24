c564d5410896a7d37e2252b3297f3ef6beeb7da8
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-07-25 00:30:44 +0530

    added code for get flight api

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -58,8 +58,29 @@ async function getAllFlights(req, res) {
     }
 }
 
+/**
+ * POST : /flights/:id
+ * req-body {}
+ */
+
+async function getFlight(req, res) {
+    try {
+        const flight = await FlightService.getFlight(req.params.id);
+        SuccessResponse.data = flight;
+        return res 
+            .status(StatusCodes.OK)
+            .json(SuccessResponse);
+    } catch(error) {
+        ErrorResponse.error = error;
+        return res 
+            .status(error.statusCode)
+            .json(ErrorResponse);
+    }
+}
+
 module.exports = {
     createFlight,
-    getAllFlights
+    getAllFlights,
+    getFlight 
 }
 
--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -16,4 +16,9 @@ router.get('/',
     FlightController.getAllFlights
 );
 
+// /api/v1/flights/:id GET 
+router.get('/:id',
+    FlightController.getFlight
+);
+
 module.exports = router;
