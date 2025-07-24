df7e062de6645fb1dea77b01f07e55f31d133df8
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-07-25 02:05:47 +0530

    started the booking implementation

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,3 +1,29 @@
-module.exports = {
-    
+const { StatusCodes } = require('http-status-codes');
+const { BookingService } = require('../services');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+
+async function createBooking(req, res) {
+    try {
+        console.log("body", req.body);
+        const response = await BookingService.createBooking({
+            flightId: req.body.flightId,
+            userId: req.body.userId,
+            noOfSeats: req.body.noOfSeats
+        });
+        SuccessResponse.data = response;
+        return res 
+            .status(StatusCodes.OK)
+            .json(SuccessResponse);
+    } catch(error) {
+        console.log("Controller catching")
+        ErrorResponse.error = error;
+        return res 
+            .status(StatusCodes.INTERNAL_SERVER_ERROR)
+            .json(ErrorResponse);
+    }
 }
+
+module.exports = {
+    createBooking
+}
+
