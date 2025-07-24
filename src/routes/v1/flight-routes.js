c2a0fe8a73cf4eba98f0c8e7d8002b14f8c576a5
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-07-25 01:26:43 +0530

    added code for flight seats update api

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -21,4 +21,10 @@ router.get('/:id',
     FlightController.getFlight
 );
 
+// /api/v1/flights/:id/seats PATCH 
+router.patch('/:id/seats',
+    FlightMiddlewares.validateUpdateSeatsRequest,
+    FlightController.updateSeats
+);
+
 module.exports = router;
