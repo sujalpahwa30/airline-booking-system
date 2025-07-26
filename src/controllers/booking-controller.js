f0242f5d8234dbf1eadc3ff3cd88a38d13063d45
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-07-27 02:24:58 +0530

    added idempotency key to make payment

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -2,6 +2,8 @@ const { StatusCodes } = require('http-status-codes');
 const { BookingService } = require('../services');
 const { SuccessResponse, ErrorResponse } = require('../utils/common');
 
+const inMemDb = {};
+
 async function createBooking(req, res) {
     try {
         const response = await BookingService.createBooking({
@@ -23,6 +25,18 @@ async function createBooking(req, res) {
 
 async function makePayment(req, res) {
     try {
+        const idempotencyKey = req.headers['x-idempotency-key'];
+        if(!idempotencyKey) {
+            return res 
+                .status(StatusCodes.BAD_REQUEST)
+                .json({ message: 'Idempotency key is missing' });
+        }
+        if(inMemDb(idempotencyKey)) {
+            return res 
+                .status(StatusCodes.BAD_REQUEST)
+                .json({ message: 'Payment already made for this booking' });
+        }
+
         const response = await BookingService.makePayment({
             totalCost: req.body.totalCost,
             userId: req.body.userId,
