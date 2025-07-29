b988524bebe0e85b3004ce90737777532057eeb6
Sujal Pahwa <sujalpahwa4@gmail.com>
2025-07-29 23:41:54 +0530

    completed consumption and sending mail

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
 
+const amqplib = require("amqplib");
+const { EmailService } = require('./services');
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("notification-queue");
+        channel.consume("notification-queue", async (data) => {
+            console.log(`${Buffer.from(data.content)}`);
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            await EmailService.sendEmail("airlinenotification@gmail.com", object.recepientEmail, object.subject, object.text);
+            channel.ack(data);
+        })
+    } catch (error) {
+        console.error("Error connecting to RabbitMQ:", error);
+    }
+}
+
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
@@ -12,6 +30,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    await connectQueue();
+    console.log("Queue is up");
 });
